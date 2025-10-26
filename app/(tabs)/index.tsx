7e5aa79a08265e4b5f5cbc6af11c32616a6da404
Adonis <adonis@spls.ae>
2025-10-26 17:29:57 +0400

    ADD snapPoints

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,45 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import React, { useCallback, useMemo, useRef } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function TabOneScreen() {
+  const snapPoints = useMemo(() => ["25%", "50%", "70%", "100%"], []);
+  // ref
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const handleClosePress = () => bottomSheetRef.current?.close();
+  const handleOpenPress = () => bottomSheetRef.current?.expand();
+  const handleCollapsePress = () => bottomSheetRef.current?.collapse();
+  const snapToIndex = (index: number) => {
+    bottomSheetRef.current?.snapToIndex(index);
+  };
+
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
+  // renders
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <GestureHandlerRootView style={styles.container}>
+        <Button title="Open Bottom Sheet" onPress={handleOpenPress} />
+        <Button title="Close Bottom Sheet" onPress={handleClosePress} />
+        <Button title="Collapse Bottom Sheet" onPress={handleCollapsePress} />
+        <Button title="Snap to 0" onPress={() => snapToIndex(0)} />
+        <Button title="Snap to 1" onPress={() => snapToIndex(1)} />
+        <Button title="Snap to 2" onPress={() => snapToIndex(2)} />
+        <Button title="Snap to 3" onPress={() => snapToIndex(3)} />
+        <BottomSheet
+          snapPoints={snapPoints}
+          ref={bottomSheetRef}
+          onChange={handleSheetChanges}
+        >
+          <BottomSheetView style={styles.contentContainer}>
+            <Text>Awesome 🎉</Text>
+          </BottomSheetView>
+        </BottomSheet>
+      </GestureHandlerRootView>
     </View>
   );
 }
@@ -16,16 +47,11 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "grey",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  contentContainer: {
+    flex: 1,
+    padding: 36,
+    alignItems: "center",
   },
 });
